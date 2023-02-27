3c24450bda1e651f031038f0ecf6d182a48d651e
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-02-26 18:00:15 -0800

    started adding to index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
-const generateHTML = require("./")
+const generateHTML = require("./generateHTML");
+
+// Importing all classes
+const Employee = require('./lib/Employee');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+
+// Declare employees as an empty array to be filled with user input
+const employees = [];
+
