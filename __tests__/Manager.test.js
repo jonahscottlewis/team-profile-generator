6db0525baa8e3da9378e35f9f1a207abc3e492ac
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-03-02 23:45:58 -0800

    added to tests index.js and generatehtml

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,48 @@
 const Employee = require('../lib/Employee');
 const Manager = require('../lib/Manager');
 
-describe('', () =>{
-    
-})
+describe('Manager', () => {
+    const exampleManager = {
+        name: "Bob",
+        id: 4,
+        email: "bob@mail.com",
+        officeNumber: 1,
+    };
+    describe("constructor tests", () => {
+        test("should construct a new instance of manager class", () => {
+            const manager = new Manager(exampleManager);
+            expect(manager).toBeinstanceOf(Manager);
+        });
+        test("should create new instance of manager class", () => {
+            const manager = new Manager(exampleManager);
+            expect(manager).toEqual({
+                name: "Bob",
+                id: 4,
+                email: "bob@mail.com",
+                officeNumber: 1,
+            });
+        });
+    });
+    describe("method tests", () => {
+        test("name should be returned when getName method is called", () => {
+            const manager = new Manager(exampleManager);
+            expect(manager.getName()).toEqual("Bob");
+        });
+        test("id should be returned when getId method is called", () => {
+            const manager = new Manager(exampleManager);
+            expect(manager.getId()).toEqual(4);
+        });
+        test("email should be returned when getEmail method is called", () => {
+            const manager = new Manager(exampleManager);
+            expect(manager.getEmail()).toEqual("bob@mail.com");
+        });
+        test("office number should be returend when getOfficeNumber method is called", () => {
+            const manager = new Manager(exampleManeger);
+            expect(manager.getOfficeNumber()).toEqual("1");
+        });
+        test("intern role should be returned when getRole method is called", () => {
+            const manager = new Manager(exampleManager);
+            expect(manager.getRole().toEqual("manager"))
+        });
+    });
+});
--- index.js
+++ index.js
@@ -11,3 +11,176 @@ const Manager = require('./lib/Manager');
 // Declare employees as an empty array to be filled with user input
 const employees = [];
 
+let isTeamComplete = false;
+
+const validateInput = (userInput) => {
+    if (userInput === "") {
+        return "please give an anser to proceed";
+    } else {
+        return true;
+    }
+};
+
+const init = async () => {
+    await createManager();
+
+    while (!isTeamComplete) {
+        const employeeQuestion = [
+            {
+                type: "list",
+                message: "Please select employee type",
+                name: "employeeType",
+                choices: [
+                    { name: "Engineer", value: "engineer", short: "Engineer" },
+                    { name: "Intern", value: "Intern", short: "None" },
+                    { name: "None", value: "None", short: "None" },
+                ],
+
+            }
+        ]
+
+        const { employeeType } = await inquirer.createPromptModule(employeeQuestion);
+
+        if (employeeType === "none") {
+            isTeamComplete = true;
+
+
+        } else {
+            if (employeeType === "engineer") {
+                await createEngineer();
+            }
+            if (employeeType === "intern") {
+                await createIntern();
+            }
+        }
+    }
+
+
+// The employees array is passed into the generate html function and then this markdown is used to create a new team-profile.html
+    const HTML = generateHTML(employees);
+    fswriteFileSync("team-profile.html",HTML, (err) => {
+        // Errors are caught
+        if(err){
+            console.log(err);
+
+        // If writing to file was succesful, then the HTML file created message is logged in the console
+        }else{
+            console.log("HTML file created");
+        }
+    });
+
+};
+
+const createManager = async () =>{
+
+    const managerQuestions = [
+        {
+            type: "input",
+            message: "Enter manager name",
+            name: "name",
+            validate: validateInput,
+        },
+        {
+            type: "input",
+            message: "Enter employee ID number",
+            name: "ID",
+            validate: validateInput,
+        },
+        {
+            type: "input",
+            message: "Enter manager office number",
+            name: "officeNumber",
+            validate: validateInput,
+        },
+        {
+            type: "input",
+            message: "Enter e-mail",
+            name: "email",
+            validate: validateInput,
+        },
+
+    ];
+
+
+    const managerAnswers = await inquirer.createPromptModule(managerQuestions);
+
+    const manager = new Manager(managerAnswers);
+
+    employees.push(manager);
+
+};
+
+const createEngineer = async () => {
+
+    const engineerQuestions = [
+        {
+            type: "input",
+            message: "Enter engineer name",
+            name: "name",
+            validate: validateInput,
+        },
+        {
+            type: "input",
+            message: "Enter engineer ID number",
+            name:"ID",
+            validate: validateInput,
+        },
+        {
+            type: "input",
+            message: "Enter email",
+            name: "email",
+            validate: validateInput,
+        },
+        {
+            type: "input",
+            message: "Enter engineer GitHub URL",
+            name: "github",
+            validate: validateInput,
+        }
+    ];
+
+    const engineerAnswers = await inquirer.prompt(engineerQuestions);
+
+    const engineer = new Engineer(engineerAnswers);
+
+    employees.push(engineer);
+
+};
+
+const createIntern = async () => {
+
+    const internQuestions = [
+        {
+            type: "input",
+            message: "Enter intern name",
+            name: "name",
+            valiadate: validateInput,
+        },
+        {
+            type: "input",
+            message: "enter intern ID number",
+            name: "ID",
+            validate: validateInput,
+        },
+        {
+            type: "input",
+            message: "Enter intern e-mail",
+            name: "email",
+            validate: validateInput,
+        },
+        {
+            type: "input",
+            message: "Enter intern school",
+            name: "scohool",
+            vallidate: validateInput,
+        },
+    ];
+
+    const internAnswers = await inquirer.prompt(internQuestions);
+
+    const intern = new Intern(internAnswers);
+
+    employees.push(intern);
+};
+
+init();
